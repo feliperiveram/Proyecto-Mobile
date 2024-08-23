48e8fbdf50f382d35318a76714ad5a1a5d83da3b
cetecom_avaras <134973157+feliperiveram@users.noreply.github.com>
2024-08-23 12:46:32 -0400

    avance clases

--- MyAPP/src/app/home/home.page.ts
+++ MyAPP/src/app/home/home.page.ts
@@ -9,4 +9,25 @@ export class HomePage {
 
   constructor() {}
 
+  user = {
+    username: '',
+    password: '',
+  };
+
+  validar(){
+    if (this.user.username.length != 0) {
+      if (this.user.username.length > 3 && this.user.username.length < 8) {
+        if (this.user.password.length == 4) {
+          console.log(this.user.password)
+          console.log(this.user.username)
+        }else {
+          console.log('Contraseña no tiene 4 caracteres')
+        }
+      }else {
+        console.log('Usuario no contiene entre 3 y 8 caracteres')
+      }
+    }else {
+      console.log('Usuario vacío')
+    }
+  }
 }
