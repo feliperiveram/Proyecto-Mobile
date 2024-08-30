0a6f6070da3db6011b41856c0cd92a41ca2e5d9d
cetecom_avaras <cetecom_avaras@duoc.cl>
2024-08-30 12:37:42 -0400

    view/selection

--- MyAPP/src/app/app-routing.module.ts
+++ MyAPP/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'selection',
+    loadChildren: () => import('./View/selection/selection.module').then( m => m.SelectionPageModule)
   },
+
 ];
 
 @NgModule({
