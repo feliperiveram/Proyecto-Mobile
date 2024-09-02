65360fb36858ff2ec1eab1c2d403375ffc292784
cetecom_avaras <134973157+feliperiveram@users.noreply.github.com>
2024-09-02 14:55:37 -0400

    Avance 02.09 (Vista chofer)
    - Avance vista chofer

--- MyAPP/src/app/View/chofer/chofer.page.ts
+++ MyAPP/src/app/View/chofer/chofer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chofer',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chofer.page.scss'],
 })
 export class ChoferPage implements OnInit {
-
-  constructor() { }
+  username = '';
+  constructor(private router: Router) { 
+    const navegacion = this.router.getCurrentNavigation();
+    const state = navegacion?.extras.state as {
+      username: '';
+      password: '';
+    };
+    this.username = state.username;
+  }
 
   ngOnInit() {
   }
