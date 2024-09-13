98ea2a0e383f87d305d134353bad25605c1a5734
cetecom_avaras <cetecom_avaras@duoc.cl>
2024-09-13 12:44:09 -0300

    avance clase

--- MyAPP/src/app/registro/registro.page.ts
+++ MyAPP/src/app/registro/registro.page.ts
@@ -12,15 +12,41 @@ export class RegistroPage implements OnInit {
   usuario = new FormGroup({
     nombre: new FormControl(''),
     clave: new FormControl(''),
+    cclave: new FormControl(''),
   });
 
-
+  mensaje = '';
 
   constructor(private router: Router) { }
 
-  guardarDatos(){
-    console.log(this.usuario.value);
-    this.router.navigate(['/home']);
+  Registrarse(){
+    if (this.usuario.controls.nombre.value?.length != 0){
+      if (this.usuario.controls.clave.value?.length !=0) {
+        if (this.usuario.controls.nombre.value?.length && this.usuario.controls.nombre.value?.length >= 3 &&  this.usuario.controls.nombre.value?.length <= 8) {
+          if (this.usuario.controls.clave.value?.length == 4) {
+            if (this.usuario.controls.clave.value == this.usuario.controls.cclave.value) {
+              console.log(this.usuario.value);
+              this.router.navigate(['/home']);              
+            }
+            else {
+              this.usuario.controls.cclave.setValue('malisimo')
+            }
+          }
+          else {
+            this.usuario.controls.clave.setValue('terrible');
+          }
+        }
+        else {
+          this.usuario.controls.nombre.setValue('mal');
+        }
+      }
+      else {
+        this.usuario.controls.clave.setValue('malisimo');
+      }
+    }
+    else {
+      this.usuario.controls.nombre.setValue('esta mal');
+    }
   }
 
   ngOnInit() {
